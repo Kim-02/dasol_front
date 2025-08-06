56b889545e92b12d4f243ca73e057b9219196125
DEUS <pwg12377@gmail.com>
2025-08-06 17:46:23 +0900

    이벤트 페이지 초안(요구사항 미반영)

---
+++ src/pages/board/eventBoard.jsx
@@ -0,0 +1,204 @@
+import React,{ useState, useEffect } from "react";
+import './documentBoard.css'
+import { loadUserInfo } from "../../utils/auth";
+import { useNavigate, Link} from "react-router-dom";
+import { handleLogout, toggleDropdown, handleInputChange, cancelEdit} from "../../utils/boardUtils";
+import { loadEvents, handleView, handleSave } from "../../utils/eventUtils";
+
+
+function EventBoard(){
+    const [userInfo, setUserInfo] = useState("로딩 중...");
+    const [dropdownboard, setDropdownBoard] = useState(false); 
+    const [dropdownApproval, setDropdownApproval] = useState(false);
+    const navigate = useNavigate();
+
+    const [searchEvents, setSearchEvents] = useState("");
+    const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [viewEvent, setViewEvent] = useState(null);
+    const [editMode, setEditMode] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+
+    const filteredEvents= events.filter(event => event.title.toLowerCase().includes(searchEvents.toLowerCase()) || event.content?.toLowerCase().includes(searchEvents.toLowerCase()));
+    
+    useEffect(() => {
+        // 페이지 로드 시 정보 가져옴
+        loadUserInfo()
+        .then(result => {
+            if (!result) throw new Error("사용자 정보 없음");
+            setUserInfo(`${result.name || '이름 없음'} (${result.studentId || '학번 없음'})`);
+        })
+        .catch(err => {
+            alert("로그인이 필요함");
+            /* navigate('/'); */
+        });
+
+        loadEvents(setLoading, setEvents);
+    }, []);
+    
+
+    return(
+        <div className="main-wrapper">
+            {/*사이드바*/}
+            <nav className="sidebar">
+                <ul>
+                <li><Link to="/main" className="sidebar-link">대시보드</Link></li>
+
+               {/* 게시판 드롭다운 */}
+                <li className="dropdown">
+                    <div className="dropdown-toggle" onClick={() => toggleDropdown(setDropdownBoard)}>게시판 <span className="arrow">
+                        {dropdownboard ? "▲" : "▼"}</span>
+                    </div>
+                    {dropdownboard && (
+                        <ul className={`dropdown-menu ${dropdownboard ? 'show' : ''}`}>
+                            <li><Link to="/document_board">문서게시판</Link></li>
+                            <li><Link to="/event_board">이벤트게시판</Link></li>
+                            <li><Link to="/inquiry_board.html">문의게시판</Link></li>
+                        </ul>
+                    )}
+                </li>
+                <li className="dropdown">
+                    <div className="dropdown-toggle" onClick={() => toggleDropdown(setDropdownApproval)}>결재<span className="arrow">
+                        {dropdownApproval ? "▲" : "▼"}</span>
+                    </div>
+                    {dropdownApproval && (
+                        <ul className={`dropdown-menu ${dropdownApproval ? 'show' : ''}`}>
+                            <li><Link to="/approval_request" className="sidebar-link">결재 신청</Link></li>
+                            <li><Link to="/approval_process" className="sidebar-link">결재 처리</Link></li>
+                        </ul>
+                    )}
+                </li>
+                <li><Link to="/monthly_summary" className="sidebar-link">월별 결산</Link></li>
+                <li><Link to="/" className="sidebar-link">설정</Link></li>
+                </ul>
+            </nav>
+
+            {/* 메인 영역 */}
+            <div className="main">
+                {/* 헤더: 우측 상단 사용자 정보 */}
+                <header className="header">
+                {/* auth.js가 자동으로 이 요소를 채웁니다 */}
+                <div className="user-info" style={{cursor: "pointer"}} onClick={() => navigate("/user")}>
+                    {userInfo}
+                </div>
+                <button id="logoutBtn" className="logout-btn" onClick={() => handleLogout(navigate)}>로그아웃</button>
+                </header>
+
+                {/* 본문: 게시판 내용 */}
+                <section className="content">
+                    <div className="board-header">
+                        <h1>이벤트 게시판</h1>
+                        <button className="btn-create" onClick={() => navigate('/event_create')}>이벤트 생성</button>
+                    </div>
+
+                <div className="search-section">
+                    <div className="search-box">
+                    <input type="text" id="searchInput" value={searchEvents} onChange={(e) => setSearchEvents(e.target.value)} placeholder="제목 또는 내용으로 검색..."></input>
+                    <button className="btn-search" onClick={() => {if (!searchEvents.trim()){alert("검색어를 입력하세요.")}}}>검색</button>
+                    </div>
+                </div>
+
+                {loading ? (
+                    <div className="loading">이벤트를 불러오는 중...</div>
+                ):(
+                    <div className="events-container">
+                        <div className="events-header">
+                        <div className="col-title">제목</div>
+                        <div className="col-author">작성자</div>
+                        <div className="col-capacity">인원</div>
+                        <div className="col-target">대상</div>
+                        <div className="col-payAmount">참가비</div>
+                        <div className="col-startdate">시작일시</div>
+                        <div className="col-enddate">종료일시</div>
+                    </div>
+                    <div id="eventsList" className="events-list">
+                        {filteredEvents.length === 0 ? (
+                            <div className="no-data">등록된 이벤트가 없습니다.</div>
+                        ) : filteredEvents.map(event => (
+                            <div className="post-row" key={event.id} onClick={() => handleView(event.id, setViewEvent, setEditMode, setShowModal)}>
+                                <div className="col-title">{event.title}</div>
+                                <div className="col-author">{event.memberName}</div>
+                                <div className="col-capacity">{event.capacity}</div>
+                                <div className="col-target">{event.target}</div>
+                                <div className="col-payAmount">{event.payAmount}</div>
+                                <div className="col-startdate">{new Date(event.startDate).toLocaleString()}</div>
+                                <div className="col-enddate">{new Date(event.endDate).toLocaleString()}</div>
+                            </div>
+                        ))}
+                        </div>
+                    </div>
+                )}
+            </section>
+
+            {showModal && viewEvent &&(
+                <div className="modal">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h2>{editMode ? '수정' : '상세 보기'}</h2>
+                            <span className="close" onClick={() => setShowModal(false)}>&times;</span>
+                        </div>
+                        <div className="modal-body">
+                            {!editMode ? (
+                                <div>
+                                    <p><strong>작성자:</strong> {viewEvent.memberName}</p>
+                                    <p><strong>인원:</strong> {viewEvent.capacity}</p>
+                                    <p><strong>대상:</strong> {viewEvent.target}</p>
+                                    <p><strong>내용:</strong> {viewEvent.content}</p>
+                                    <p><strong>시작일시:</strong> {new Date(viewEvent.startDate).toLocaleString()}</p>
+                                    <p><strong>종료일시:</strong> {new Date(viewEvent.endDate).toLocaleString()}</p>
+                                    <p><strong>참가비:</strong> {viewEvent.payAmount}</p>
+
+                                    <div className="modal-footer">
+                                        <button className="btn-edit" onClick={() => setEditMode(true)}>수정</button>
+                                    </div>
+                                </div>
+                            ) : (
+                                <form>
+                                    <div className="form-group">
+                                        <label>제목</label>
+                                        <input type="text" name="title" value={viewEvent.title} onChange={(e) => handleInputChange(e, setViewEvent)} required/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>내용</label>
+                                        <textarea name="content" value={viewEvent.content} onChange={(e) => handleInputChange(e, setViewEvent)} rows="4" required></textarea>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>시작일시</label>
+                                        <input type="datetime-local" name="startDate" value={viewEvent.startDate ? viewEvent.startDate.slice(0, 16) : ''} onChange={(e) => handleInputChange(e, setViewEvent)} required/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>종료일시</label>
+                                        <input type="datetime-local" name="endDate" value={viewEvent.endDate ? viewEvent.endDate.slice(0, 16) : ''} onChange={(e) => handleInputChange(e, setViewEvent)} required/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>대상</label>
+                                        <input type="text" name="target" value={viewEvent.target} onChange={(e) => handleInputChange(e, setViewEvent)} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>인원</label>
+                                        <input type="number" name="capacity" value={viewEvent.capacity} onChange={(e) => handleInputChange(e, setViewEvent)} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>참가비</label>
+                                        <input type="number" name="payAmount" value={viewEvent.payAmount} onChange={(e) => handleInputChange(e, setViewEvent)} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>공지사항 여부</label>
+                                        <input type="checkbox" name="notice" checked={viewEvent.notice} onChange={(e) => setViewEvent(prev => ({...prev, notice: e.target.checked}))} />
+                                    </div>
+                                    <div className="form-action">
+                                        <button type="button" className="btn-cancel" onClick={() => cancelEdit(setEditMode)}>취소</button>
+                                        <button type="button" className="btn-submit" onClick={() => handleSave(viewEvent, setShowModal, setViewEvent, setLoading, setEvents)}>저장</button>
+                                    </div>
+                                </form>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            )}
+            </div>
+        </div>
+    );
+}
+
+export default EventBoard;
